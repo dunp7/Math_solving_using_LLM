3a9ad6793696d7f8e54ad94e81e8ed2fb26e2cc4
Phạm Tiến Dũng  (HN-IID-Sales Executive) <dungpt6@runsystem.local>
2025-03-28 09:40:42 +0700

    upd Prompt

--- my_utils/metrics.py
+++ my_utils/metrics.py
@@ -18,16 +18,24 @@ def assess_acc(model, tokenizer, question, answers, response):
         int: Returns 1 if the model determines the response is equivalent to the expected answer, otherwise 0
     """
 
-    prompt = (f"We are assessing the quality of answers to the following question: {question}\n"
-              f"The expected answer is: {answers}\n"
-              f"The proposed answer is: {response}\n"
-              f"Within the context of the question, does the proposed answer mean the same as the expected answer?\n"
-              f"Respond only with yes or no.")
-    
+    # prompt = (f"We are assessing the quality of answers to the following question: {question}\n"
+    #           f"The expected answer is: {answers}\n"
+    #           f"The proposed answer is: {response}\n"
+    #           f"Within the context of the question, does the proposed answer mean the same as the expected answer?\n"
+    #           f"Respond only with yes or no.")
+    prompt = (f"Question: {question}\n"
+            f"Expected answer: {answers}\n"
+            f"Proposed answer: {response}\n"
+            "Does the proposed answer convey the same meaning as the expected answer? "
+            "Answer 'yes' if they are semantically equivalent and 'no' if they are not. "
+            "Be precise in your evaluation and answer only with 'yes' or 'no'.")
+
+
     acc_input_ids = tokenizer(prompt, return_tensors="pt").to(model.device)
     input_length = acc_input_ids["input_ids"].shape[1]
     output = model.generate(**acc_input_ids, max_new_tokens=256, return_dict_in_generate=True)
     text_res = tokenizer.decode(output.sequences[0, input_length:], skip_special_tokens=True).lower()
+    
     
     return 1 if "yes" in text_res else 0
 
